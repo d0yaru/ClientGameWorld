08514d1df85691f7191862578a4e4ee7744da935
Dmitry <d0@ya.ru>
2021-12-06 19:09:56 +0300

    v0.1.12.06.3

--- source_code/Engine/systemclass.cpp
+++ source_code/Engine/systemclass.cpp
@@ -197,9 +197,10 @@ void SystemClass::InitializeWindows(int& screenWidth, int& screenHeight)
 	}
 
 	// Create the window with the screen settings and get the handle to it.
-	m_hwnd = CreateWindowEx(WS_EX_APPWINDOW, m_applicationName, m_applicationName, 
+	/*m_hwnd = CreateWindowEx(WS_EX_APPWINDOW, m_applicationName, m_applicationName,
 						    WS_CLIPSIBLINGS | WS_CLIPCHILDREN | WS_POPUP,
-						    posX, posY, screenWidth, screenHeight, NULL, NULL, m_hinstance, NULL);
+						    posX, posY, screenWidth, screenHeight, NULL, NULL, m_hinstance, NULL);*/
+	m_hwnd = CreateWindowEx(WS_EX_APPWINDOW, m_applicationName, m_applicationName, WS_OVERLAPPEDWINDOW | WS_VISIBLE, posX, posY, screenWidth, screenHeight, NULL, NULL, m_hinstance, NULL);
 
 	// Bring the window up on the screen and set it as main focus.
 	ShowWindow(m_hwnd, SW_SHOW);
